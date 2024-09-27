552c49d5feeba726ec2bb5782d7d3dbd1165436a
ruairi92020 <ruairi.92020@gmail.com>
2024-09-27 20:26:42 +0000

    i made tina make a tiangle

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -19,5 +19,15 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # tina.pencolor()
 
 ... # Your code here
+tina.pencolor('red')
+tina.forward(100)
+tina.left(120)
 
+tina.pencolor('green')
+tina.forward(100)
+tina.left(120)
+
+tina.pencolor('blue')
+tina.forward(100)
+tina.left(120)
 turtle.exitonclick()                    # Close the window when we click on it
